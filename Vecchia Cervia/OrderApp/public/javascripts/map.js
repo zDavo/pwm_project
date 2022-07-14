044fa2883eb1f42f5721be69366f3d1fb48486e7
Davide Massini <72620139+zDavo@users.noreply.github.com>
2022-07-14 11:31:54 +0200

    Riorganizzazione cartelle

--- Vecchia Cervia/OrderApp/public/javascripts/map.js
+++
@@ -1,8 +0,0 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 44.261965, lng: 12.348104 },
-    zoom: 16
-  });
-  var marker = new google.maps.Marker({lat: 44.261965, lng: 12.348104, map: map});
-}
